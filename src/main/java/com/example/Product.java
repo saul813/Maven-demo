28340b7e2e62af56602beaa12c6173ea50aef175
saul813 <saulsabunyo8@gmail.com>
2026-07-04 14:03:42 +0300

    Add Product class containing the annotations

--- src/main/java/com/example/Product.java
+++ src/main/java/com/example/Product.java
@@ -1,4 +1,32 @@
 package com.example;
-
+import jakarta.persistence.*;
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private double price;
+
+    //Required by Hibernate
+    public Product() {}
+
+    public Product(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    //Getters and Setters
+    public Long getId(){ return id; }
+    public void setId(Long id){ this.id = id; }
+    public String getName(){ return name; }
+    public void setName(String name){ this.name = name; }
+    public double getPrice(){return price; }
+    public void setPrice(double price){ this.price = price; }
 }
